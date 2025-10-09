5778c98e8b2bac839ee3cff1cf2ff0a73c661699
John Wood <a02324885@usu.edu>
2025-10-09 19:58:18 +0000

    updated mini-report with knitted .md file

---
+++ Practice.R
@@ -0,0 +1,21 @@
+round(4, digits = 3)
+round(7.50000, digits = 1)
+
+?round
+
+x <- 7.1985712059817
+
+round(x)
+
+?mean
+
+mean(c(3,2,4,5,2,5,3,5,3,2,6), trim=0.4, na.rm=TRUE)
+
+?read.csv
+
+data <- read.csv(file="bloodmeal_for_BIOL3070.csv")
+
+head(data)
+View(data)
+str(data)
+dim(data) #dimensions
